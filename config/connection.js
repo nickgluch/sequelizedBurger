b53616492ec9fcacb86f6d95a4e655c35cfcf151
Nick Gluch <nickrgluch@yahoo.com>
2019-09-12 20:06:46 -0500

    added skeleton

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+
+        // Your port; if not 3306
+        port: 3306,
+
+        // Your username
+        user: "root",
+
+        // Your password
+        password: "",
+        database: "burgers_db"
+    });
+}
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = (connection);
